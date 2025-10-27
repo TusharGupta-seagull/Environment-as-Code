5ed1e0c1fc39af344be3916b59d669407f174971
TusharGupta-seagull <guptatushar015@gmail.com>
2025-10-27 09:48:20 +0530

    add-output

--- network/security_group/output.tf
+++ network/security_group/output.tf
@@ -1,14 +1,34 @@
 output "security_group_id" {
-  description = "The ID of the created or existing security group"
+  description = "ID of the security group"
   value       = local.create_sg ? aws_security_group.main[0].id : null
 }
 
 output "security_group_arn" {
-  description = "The ARN of the created or existing security group"
+  description = "ARN of the security group"
   value       = local.create_sg ? aws_security_group.main[0].arn : null
 }
 
 output "security_group_name" {
-  description = "The name of the security group"
-  value       = local.create_sg ? aws_security_group.main[0].name : var.sg_name
+  description = "Name of the security group"
+  value       = local.create_sg ? aws_security_group.main[0].name : null
+}
+
+output "security_group_vpc_id" {
+  description = "VPC ID of the security group"
+  value       = local.create_sg ? aws_security_group.main[0].vpc_id : null
+}
+
+output "security_group_owner_id" {
+  description = "Owner ID of the security group"
+  value       = local.create_sg ? aws_security_group.main[0].owner_id : null
+}
+
+output "ingress_rule_ids" {
+  description = "IDs of the ingress rules"
+  value       = local.create_sg ? { for k, v in aws_vpc_security_group_ingress_rule.this : k => v.id } : {}
+}
+
+output "egress_rule_ids" {
+  description = "IDs of the egress rules"
+  value       = local.create_sg ? { for k, v in aws_vpc_security_group_egress_rule.this : k => v.id } : {}
 }
