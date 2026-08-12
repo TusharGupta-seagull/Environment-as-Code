ad4c7f2f38700e635f3ce915d67b2dc9fc99b4a2
TusharGupta-seagull <tushargupta.dev@gmail.com>
2026-08-12 12:08:06 +0530

    fix: point security group module outputs at this resource

--- terraform/_modules/network/security_group/output.tf
+++ terraform/_modules/network/security_group/output.tf
@@ -1,26 +1,26 @@
 output "security_group_id" {
   description = "ID of the security group"
-  value       = var.create_sg ? aws_security_group.main[0].id : null
+  value       = var.create_sg ? aws_security_group.this[0].id : null
 }
 
 output "security_group_arn" {
   description = "ARN of the security group"
-  value       = var.create_sg ? aws_security_group.main[0].arn : null
+  value       = var.create_sg ? aws_security_group.this[0].arn : null
 }
 
 output "security_group_name" {
   description = "Name of the security group"
-  value       = var.create_sg ? aws_security_group.main[0].name : null
+  value       = var.create_sg ? aws_security_group.this[0].name : null
 }
 
 output "security_group_vpc_id" {
   description = "VPC ID of the security group"
-  value       = var.create_sg ? aws_security_group.main[0].vpc_id : null
+  value       = var.create_sg ? aws_security_group.this[0].vpc_id : null
 }
 
 output "security_group_owner_id" {
   description = "Owner ID of the security group"
-  value       = var.create_sg ? aws_security_group.main[0].owner_id : null
+  value       = var.create_sg ? aws_security_group.this[0].owner_id : null
 }
 
 output "ingress_rule_ids" {
