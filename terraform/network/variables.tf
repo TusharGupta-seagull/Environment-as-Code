6435efa40958bc66f9aec85ae8c26a0931083370
TusharGupta-seagull <tushargupta.dev@gmail.com>
2026-08-12 12:09:03 +0530

    refactor: require network config from root instead of defaults

--- terraform/network/variables.tf
+++ terraform/network/variables.tf
@@ -34,39 +34,4 @@ variable "network_config" {
     })
   })
 
-  default = {
-    vpc = {
-      cidr = "10.0.0.0/16"
-    }
-
-    subnets = {
-      public = [
-        {
-          cidr       = "10.0.1.0/24"
-          avail_zone = "us-east-1a"
-        },
-        {
-          cidr       = "10.0.2.0/24"
-          avail_zone = "us-east-1b"
-        }
-      ]
-
-      private = [
-        {
-          cidr             = "10.0.10.0/24"
-          enable_nat_route = true
-          avail_zone       = "us-east-1b"
-        }
-      ]
-    }
-
-    settings = {
-      map_public_ip_on_launch = {
-        pub_sub  = true
-        priv_sub = false
-      }
-      create_alb       = true
-      allowed_ssh_cidr = "0.0.0.0/0"
-    }
-  }
 }
