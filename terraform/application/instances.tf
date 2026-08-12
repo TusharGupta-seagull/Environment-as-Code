44eb369704816c325c80bf50050c867e8fad65bb
TusharGupta-seagull <tushargupta.dev@gmail.com>
2026-08-12 12:18:36 +0530

    refactor: drop standalone app and db instances from application module

--- terraform/application/instances.tf
+++ terraform/application/instances.tf
@@ -1,72 +1,28 @@
 locals {
   ec2_config = {
-
     bastion = {
-      count         = lookup(var.ec2_config.bastion, "count", 0)
-      instance_type = lookup(var.ec2_config.bastion, "instance_type", "t3.micro")
+      count         = var.ec2_config.bastion.count
+      instance_type = var.ec2_config.bastion.instance_type
       key_name      = var.ec2_config.key_name
 
-      subnet_id = lookup(var.ec2_network_config.bastion, "subnet_id", null)
-      sg_ids    = lookup(var.ec2_network_config.bastion, "sg_ids", [])
+      subnet_id = var.ec2_network_config.bastion.subnet_id
+      sg_ids    = var.ec2_network_config.bastion.sg_ids
 
-      ami_id                      = lookup(var.ec2_config.bastion, "ami_id", null)
-      user_data                   = lookup(var.ec2_config.bastion, "user_data", null)
-      availability_zone           = lookup(var.ec2_config.bastion, "availability_zone", null)
-      associate_public_ip_address = lookup(var.ec2_config.bastion, "associate_public_ip_address", true)
+      # Bastion always runs Amazon Linux (AL2023 resolved via SSM)
+      ami_id_ssm_parameter        = var.default_ami_id_ssm_parameter
+      user_data                   = var.ec2_config.bastion.user_data
+      availability_zone           = var.ec2_config.bastion.availability_zone
+      associate_public_ip_address = var.ec2_config.bastion.associate_public_ip_address
 
-      root_block_device = lookup(var.ec2_config.bastion, "root_block_device", {
+      root_block_device = coalesce(var.ec2_config.bastion.root_block_device, {
         delete_on_termination = true
         volume_type           = "gp3"
         volume_size           = 10
         encrypted             = true
       })
     }
-
-    app = {
-      count         = lookup(var.ec2_config.app, "count", 0)
-      instance_type = lookup(var.ec2_config.app, "instance_type", "t3.small")
-      key_name      = var.ec2_config.key_name
-
-      subnet_id = lookup(var.ec2_network_config.app, "subnet_id", null)
-      sg_ids    = lookup(var.ec2_network_config.app, "sg_ids", [])
-
-      ami_id                      = lookup(var.ec2_config.app, "ami_id", null)
-      user_data                   = lookup(var.ec2_config.app, "user_data", null)
-      availability_zone           = lookup(var.ec2_config.app, "availability_zone", null)
-      associate_public_ip_address = lookup(var.ec2_config.app, "associate_public_ip_address", false)
-
-      root_block_device = lookup(var.ec2_config.app, "root_block_device", {
-        delete_on_termination = true
-        volume_type           = "gp3"
-        volume_size           = 10
-        encrypted             = true
-      })
-    }
-
-    db = {
-      count         = lookup(var.ec2_config.db, "count", 0)
-      instance_type = lookup(var.ec2_config.db, "instance_type", "t3.small")
-      key_name      = var.ec2_config.key_name
-
-      subnet_id = lookup(var.ec2_network_config.db, "subnet_id", null)
-      sg_ids    = lookup(var.ec2_network_config.db, "sg_ids", [])
-
-      ami_id                      = lookup(var.ec2_config.db, "ami_id", null)
-      user_data                   = lookup(var.ec2_config.db, "user_data", null)
-      availability_zone           = lookup(var.ec2_config.db, "availability_zone", null)
-      associate_public_ip_address = lookup(var.ec2_config.db, "associate_public_ip_address", false)
-
-      root_block_device = lookup(var.ec2_config.db, "root_block_device", {
-        delete_on_termination = true
-        volume_type           = "gp3"
-        volume_size           = 10
-        encrypted             = true
-      })
-    }
-
   }
 }
-
 
 module "bastion_instances" {
   source = "../_modules/compute/ec2-instance"
@@ -80,61 +36,13 @@ module "bastion_instances" {
   key_name                    = local.ec2_config.bastion.key_name
   vpc_security_group_ids      = local.ec2_config.bastion.sg_ids
   associate_public_ip_address = local.ec2_config.bastion.associate_public_ip_address
-  ami_id                      = local.ec2_config.bastion.ami_id
+  ami_id_ssm_parameter        = local.ec2_config.bastion.ami_id_ssm_parameter
   user_data                   = local.ec2_config.bastion.user_data
   availability_zone           = local.ec2_config.bastion.availability_zone
   root_block_device           = local.ec2_config.bastion.root_block_device
 
   tags = var.project_config.tags
   instance_tags = {
-    Role        = "jump-server"
-    Environment = var.project_config.env_name
-  }
-}
-
-module "app_instances" {
-  source = "../_modules/compute/ec2-instance"
-  count  = local.ec2_config.app.count
-
-  create = true
-  name   = "${local.name_prefix}-app-${count.index + 1}"
-
-  instance_type               = local.ec2_config.app.instance_type
-  subnet_id                   = local.ec2_config.app.subnet_id
-  key_name                    = local.ec2_config.app.key_name
-  vpc_security_group_ids      = local.ec2_config.app.sg_ids
-  associate_public_ip_address = local.ec2_config.app.associate_public_ip_address
-  ami_id                      = local.ec2_config.app.ami_id
-  user_data                   = local.ec2_config.app.user_data
-  availability_zone           = local.ec2_config.app.availability_zone
-  root_block_device           = local.ec2_config.app.root_block_device
-
-  tags = var.project_config.tags
-  instance_tags = {
-    Role        = "app-server"
-    Environment = var.project_config.env_name
-  }
-}
-
-module "db_instances" {
-  source = "../_modules/compute/ec2-instance"
-  count  = local.ec2_config.db.count
-
-  create = true
-  name   = "${local.name_prefix}-db-${count.index + 1}"
-
-  instance_type               = local.ec2_config.db.instance_type
-  subnet_id                   = local.ec2_config.db.subnet_id
-  key_name                    = local.ec2_config.db.key_name
-  vpc_security_group_ids      = local.ec2_config.db.sg_ids
-  associate_public_ip_address = local.ec2_config.db.associate_public_ip_address
-  ami_id                      = local.ec2_config.db.ami_id
-  availability_zone           = local.ec2_config.db.availability_zone
-  root_block_device           = local.ec2_config.db.root_block_device
-
-  tags = var.project_config.tags
-  instance_tags = {
-    Role        = "db-server"
-    Environment = var.project_config.env_name
+    Component = "bastion"
   }
 }
--- terraform/application/output.tf
+++ terraform/application/output.tf
@@ -13,43 +13,29 @@ output "bastion_instances" {
   }
 }
 
-output "app_instances" {
-  description = "Application instances details"
+# Autoscaling Groups
+output "autoscaling_groups" {
+  description = "ASG details per service"
   value = {
-    for idx, instance in module.app_instances : idx => {
-      id            = instance.id
-      private_ip    = instance.private_ip
-      instance_type = instance.instance_type
-      az            = instance.availability_zone
-      state         = instance.instance_state
+    for name, asg in module.asg : name => {
+      asg_name               = asg.asg_name
+      launch_template_id     = asg.launch_template_id
+      resolved_ami_id        = asg.resolved_ami_id
+      cpu_scaling_policy_arn = asg.cpu_scaling_policy_arn
     }
   }
 }
-
-output "db_instances" {
-  description = "Database instances details"
-  value = {
-    for idx, instance in module.db_instances : idx => {
-      id            = instance.id
-      private_ip    = instance.private_ip
-      instance_type = instance.instance_type
-      az            = instance.availability_zone
-      state         = instance.instance_state
-    }
-  }
-}
-
 
 # Application Load Balancer
 output "load_balancer" {
   description = "Application Load Balancer details"
   value = {
-    enabled          = local.alb_config.create_alb
+    enabled          = var.alb_config.create_alb
     arn              = try(module.alb[0].lb_arn, null)
     dns_name         = try(module.alb[0].lb_dns_name, null)
     zone_id          = try(module.alb[0].lb_zone_id, null)
     arn_suffix       = try(module.alb[0].lb_arn_suffix, null)
-    target_group_arn = try(module.alb.target_group_arn, null)
+    target_group_arn = try(module.alb[0].target_group_arn, null)
     url              = try("http://${module.alb[0].lb_dns_name}", null)
     https_url        = try(var.alb_config.certificate_arn != null ? "https://${module.alb[0].lb_dns_name}" : null, null)
   }
